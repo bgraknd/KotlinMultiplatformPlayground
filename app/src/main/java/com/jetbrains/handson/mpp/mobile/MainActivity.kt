398dbce5338b26d2e50e48953abc90c9d7274fbf
VNGRS <vngrs@Mehmets-Air.ofis.vngrs.com>
2020-03-09 12:36:03 +0300

    Initial commit

---
+++ app/src/main/java/com/jetbrains/handson/mpp/mobile/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.jetbrains.handson.mpp.mobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        println()
+        main_text.text = createApplicationScreenMessage()
+    }
+}
